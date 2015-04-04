6e4615ff6618bf13da277325f879da3b00f16ee0
robert <gurgul.robert@gmail.com>
2015-04-04 15:16:08 +0200

    with ui-router

--- src/aside.menu.js
+++ src/aside.menu.js
@@ -1,5 +1,9 @@
+/**
+ * Created by Robert Gurgul on 2015-03-15.
+ */
+
 angular.module('aside.menu', ['menu-tpl.html', 'menu-item-tpl.html', 'ui.router'])
-    .directive('asideMenu', function ($http, $location) {
+    .directive('asideMenu', function ($http) {
         return {
             restrict: 'E',
             transclude: true,
@@ -18,7 +22,7 @@ angular.module('aside.menu', ['menu-tpl.html', 'menu-item-tpl.html', 'ui.router'
             }
         }
     })
-    .directive('menuItem', function ($state) {
+    .directive('menuItem', function ($state, $timeout) {
         return {
             restrict: 'E',
             require: '^asideMenu',
@@ -26,20 +30,30 @@ angular.module('aside.menu', ['menu-tpl.html', 'menu-item-tpl.html', 'ui.router'
             replace: true,
             templateUrl: "menu-item-tpl.html",
             link: function (scope, element, attrs, ctrl) {
-                if ($state.params.id === attrs.index) {
-                    ctrl.toggleSubmenu($(element.parent().parent()));
-                } else if (!$state.params.id && element.hasClass('active') && $(element).get(0).nextElementSibling.tagName === 'UL') {
-                    ctrl.toggleSubmenu($($(element).get(0).nextElementSibling));
-                }
+                $timeout(function () {
+                    if ($state.params.id === attrs.index && $state.includes(attrs.link)) {
+                        var ul = $(element).closest('ul');
+                        ctrl.toggleSubmenu(ul, ul.prev());
+                        element.addClass('active');
+                        ctrl.currentItem = ul.prev();
+                    } else if (!$state.params.id && $state.current.name === attrs.link) {
+                        element.addClass('active');
+                    }
+                })
+
                 scope.$state = $state;
                 element.bind('click', function (evt) {
-                    var isNotSubItem = !!$(element).next().length,
-                        mainItemWithSub = !!$(element).next().not('.ng-hide').length;
+                    if (attrs.link) {
+                        $state.go(attrs.link, {'id': attrs.index});
+                        $('.active', '.menu-main').removeClass('active');
+                        element.addClass('active');
+                    }
+                    var submenu = $(element).next(),
+                        subItem = element.hasClass('sub-item');
                     switch (true) {
-                        case isNotSubItem && mainItemWithSub: // is mainItem with submenu
-                            var submenu = $(evt.currentTarget.nextElementSibling);
+                        case !!submenu.length: // is mainItem with submenu
                             if (ctrl.activeSubmenu) {
-                                if (angular.equals(ctrl.activeSubmenu, submenu)) {
+                                if (submenu.is(ctrl.activeSubmenu)) {
                                     ctrl.toggleSubmenu(ctrl.activeSubmenu, evt.currentTarget, true);
                                 } else {
                                     ctrl.toggleSubmenu(ctrl.activeSubmenu, ctrl.currentItem);
@@ -50,7 +64,7 @@ angular.module('aside.menu', ['menu-tpl.html', 'menu-item-tpl.html', 'ui.router'
                             }
                             ctrl.currentItem = evt.currentTarget;
                             break;
-                        case isNotSubItem && !mainItemWithSub: // is mainItem without submenu
+                        case !subItem && !submenu.length: // is mainItem without submenu
                             if (ctrl.activeSubmenu) {
                                 ctrl.toggleSubmenu(ctrl.activeSubmenu, ctrl.currentItem, true);
                             }
@@ -61,12 +75,12 @@ angular.module('aside.menu', ['menu-tpl.html', 'menu-item-tpl.html', 'ui.router'
         }
     });
 
-angular.module("menu-tpl.html", []).run(["$templateCache", function ($templateCache) {
+angular.module("menu-tpl.html", []).run(function ($templateCache) {
     $templateCache.put("menu-tpl.html",
-        "<div class=\"menu-container\">\n    <div class=\"menu-main\">\n        <ul class=\'list-group list-unstyled\'>\n            <li ng-repeat=\"item in items | orderBy:\'index\':false\"\n                class=\"list-group-item menu-item-box {{ \'ind-\' + item.index }} no-select {{ item.id }}\">\n                <menu-item class=\"menu-item\"\n                           link=\"{{item.link}}\"\n                           index=\"{{item.index}}\">\n                    <i class=\"fa {{item.icon}} pull-left\"></i>\n                </menu-item>\n                <ul ng-show=\"item.submenu\"\n                    class=\"submenu list-unstyled\"\n                    style=\"display: none\">\n                    <li ng-repeat=\"item in item.submenu\">\n                        <menu-item class=\"menu-item\"\n                                   link=\"{{item.link}}\"\n                                   index=\"{{item.index}}\"\n                                   score=\"{{item.score}}\">\n                            <i class=\"fa {{item.icon}} pull-left\"></i>\n                        </menu-item>\n                    </li>\n                </ul>\n            </li>\n        </ul>\n    </div>\n</div>");
-}]);
+        "<div class=\"menu-main\">\n    <ul class=\'list-group list-unstyled\'>\n        <li ng-repeat=\"item in items\" class=\"list-group-item menu-item-box no-select\">\n            <menu-item class=\"menu-item\" link=\"{{item.link}}\" index=\"{{item.index}}\">\n                <i class=\"fa {{item.icon}} pull-left\"></i>\n            </menu-item>\n            <ul ng-if=\"item.submenu\" class=\"submenu list-unstyled\" style=\"display: none\">\n                <li ng-repeat=\"item in item.submenu\">\n                    <menu-item class=\"menu-item sub-item\" link=\"{{item.link}}\" index=\"{{item.index}}\" score=\"{{item.score}}\">\n                        <i class=\"fa {{item.icon}} pull-left\"></i>\n                    </menu-item>\n                </li>\n            </ul>\n        </li>\n    </ul>\n</div>");
+});
 
-angular.module("menu-item-tpl.html", []).run(["$templateCache", function ($templateCache) {
+angular.module("menu-item-tpl.html", []).run(function ($templateCache) {
     $templateCache.put("menu-item-tpl.html",
-        "<div ui-sref=\"{{item.link}}\" ui-sref-active=\"active\">\n    <span ng-transclude></span>\n\n    <div lass=\"menu-title\" ng-class=\"{\'pull-left\': item.submenu}\">{{ item.name }}</div>\n    <i class=\"fa fa-chevron-right pull-right\" ng-if=\"item.submenu\"></i>\n\n    <div class=\"clearfix\"></div>\n</div>");
-}]);
+        "<div>\n    <span ng-transclude></span>\n    <div class=\"menu-title\" ng-class=\"{\'pull-left\': item.submenu}\">{{ item.name }}</div>\n    <i class=\"fa fa-chevron-right pull-right\" ng-if=\"item.submenu\"></i>\n    <div class=\"clearfix\"></div>\n</div>");
+});
